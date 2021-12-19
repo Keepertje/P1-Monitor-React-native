c9c53da3db7d7751fad166b3e98e61747c8622f7
Cindy Berghuizen <cindy@skunk.team>
2021-12-19 19:14:19 +0100

    Readme

--- constants/env.js
+++ constants/env.js
@@ -1,6 +1,6 @@
 const env = {
-    p1_url : "IP_ADDRESS_METER_HERE",
-    app_name: "APP_NAME_HERE"
+    p1_url : "http://192.168.2.17/api/v1/data",
+    app_name: "Cindy's awesome P1 app"
 }
 
 export default env;
