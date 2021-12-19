1d8d6bd9931442715e852d5c646de633b02ea025
Cindy Berghuizen <cindy@skunk.team>
2021-12-19 20:10:48 +0100

    Totaal teruggeleverd

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Card from "./components/Card";
@@ -5,18 +6,22 @@ import Chart from "./components/Chart";
 import Header from "./components/Header";
 import colors from "./constants/colors";
 import env from "./constants/env";
+
 export default function App() {
   const [isCurrent, setCurrent] = useState(0);
   const [dataArray, setDataArray] = useState([0]);
+  const [terugGeleverd, setTerugGeleverd] = useState(0);
+
   const getCurrentWatt = async () => {
     try {
       // Get data from p1 meter. 
       const response = await fetch(env.p1_url);
 
-      const { active_power_w } = await response.json();
+      const { active_power_w , total_power_export_t1_kwh, total_power_export_t2_kwh} = await response.json();
 
       makeNewArray( active_power_w);
       setCurrent(active_power_w);
+      setTerugGeleverd(total_power_export_t2_kwh+total_power_export_t1_kwh);
     } catch (error) {
       console.log(error);
     }
@@ -43,17 +48,26 @@ export default function App() {
   },[]);
 
 
-  return (<View style={styles.screen}><Header/>
+  return (
+  <View style={styles.screen}>
+    <Header/>
     <Card>
-   
-   <View style={styles.textContainer}>
-     <Text style={styles.titleText}>Huidige verbruik: </Text>
-      <Text style={styles.text}>{isCurrent} Watt</Text>
-    </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.titleText}>Huidige verbruik: </Text>
+          <Text style={styles.text}>{isCurrent} Watt</Text>
+        </View>
         <Chart data={dataArray}></Chart>
        
-        </Card>
-    </View>
+    </Card>
+    <Card>
+      <View style={styles.textContainer}>
+      <Ionicons name="md-sunny" size={32} color="#2B7C80" />
+        <Text style={styles.titleText}>Teruggeleverd: </Text>
+          <Text style={styles.text}>{terugGeleverd} kWh</Text>
+        
+        </View>
+    </Card>
+  </View>
   );
 }
 
@@ -79,3 +93,4 @@ const styles = StyleSheet.create({
     fontSize:22
   },
 });
+
